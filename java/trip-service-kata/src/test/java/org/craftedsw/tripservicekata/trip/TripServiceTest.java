f5f6c2c74b490eeaead568161cffd9a4b19f03b6
nmkip <juan.ignacio848@gmail.com>
2018-09-22 19:49:25 -0300

    Removed duplication in tests.

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,6 +2,7 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -17,20 +18,22 @@ public class TripServiceTest {
     private final User LOGGED_IN_USER = new User();
     private final User CAROL = new User();
     private final Trip TO_BOLIVIA = new Trip();
+    private TripService tripService;
+
+    @Before
+    public void setUp() throws Exception {
+        tripService = new TestableTripService();
+    }
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_when_user_is_not_logged_in() {
-        TripService tripService = new TestableTripService();
-
         loggedInUser = GUEST;
 
         tripService.getTripsByUser(UNUSED_USER);
     }
 
     @Test
-    public void should_return_no_trips_when_user_is_not_a_friend() {
-        TripService tripService = new TestableTripService();
-
+    public void should_return_no_trips_when_users_are_not_a_friend() {
         loggedInUser = LOGGED_IN_USER;
 
         User anotherUser = new User();
