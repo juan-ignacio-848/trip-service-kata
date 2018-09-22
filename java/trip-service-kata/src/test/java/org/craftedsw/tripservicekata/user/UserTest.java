72f12dc84e244014da1dca357ee424f855338261
nmkip <juan.ignacio848@gmail.com>
2018-09-22 20:15:35 -0300

    Removed feature envy.
    TripService was envying User.

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,26 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class UserTest {
 
+    @Test
+    public void should_return_false_when_another_user_is_not_a_friend() {
+        User user = new User();
+        User notAFriend = new User();
+        assertThat(user.isFriendWith(notAFriend), is(false));
+    }
+
+    @Test
+    public void should_return_true_when_another_user_is_a_friend() {
+        User aFriend = new User();
+
+        User user = new User();
+        user.addFriend(aFriend);
+
+        assertThat(user.isFriendWith(aFriend), is(true));
+    }
 }
