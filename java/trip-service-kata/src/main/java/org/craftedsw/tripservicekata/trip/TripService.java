a9cab48b1f05fab84a5fc1beabc2b2d292fef440
nmkip <juan.ignacio848@gmail.com>
2018-09-26 23:28:36 -0300

    Refactored TripService

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,32 +1,33 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
-import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
-
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
-	
+    @Autowired
+    private TripDAO tripDAO;
+
+    public List<Trip> getTripsByUser(User user, User loggedInUser) throws UserNotLoggedInException {
+        if (loggedInUser == null) {
+            throw new UserNotLoggedInException();
+        }
+
+        return user.isFriendsWith(loggedInUser)
+                ? tripsBy(user)
+                : noTrips();
+    }
+
+    protected ArrayList<Trip> noTrips() {
+        return new ArrayList<Trip>();
+    }
+
+    protected List<Trip> tripsBy(User user) {
+        return tripDAO.tripsBy(user);
+    }
+
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,66 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.craftedsw.tripservicekata.trip.UserBuilder.aUser;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TripServiceTest {
-	
+
+    private final User NOT_LOGGED_IN_USER = null;
+    private final User LOGGED_IN_USER = new User();
+    private final User CARL = new User();
+
+    private final Trip TO_PERU = new Trip();
+    private final Trip TO_SPAIN = new Trip();
+
+    @Mock
+    private TripDAO tripDAO;
+
+    @InjectMocks
+    private TripService realTripService = new TripService();
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void should_throw_an_exception_when_user_is_not_logged_in() {
+        realTripService.getTripsByUser(CARL, NOT_LOGGED_IN_USER);
+    }
+
+    @Test
+    public void should_return_any_trips_when_users_are_not_friends() {
+        User bob = aUser()
+                .friendsWith(CARL)
+                .withTrips(TO_PERU)
+                .build();
+
+
+        List<Trip> friendTrips = realTripService.getTripsByUser(bob, LOGGED_IN_USER);
+        assertThat(friendTrips.size(), is(0));
+    }
+
+    @Test
+    public void should_return_friend_trips_when_users_are_friends() {
+        User bob = aUser()
+                .friendsWith(CARL, LOGGED_IN_USER)
+                .withTrips(TO_PERU, TO_SPAIN)
+                .build();
+
+        given(tripDAO.tripsBy(bob)).willReturn(bob.trips());
+
+        List<Trip> friendTrips = realTripService.getTripsByUser(bob, LOGGED_IN_USER);
+        assertThat(friendTrips.size(), is(2));
+    }
+
 }
